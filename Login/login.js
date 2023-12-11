f1e1a438ac840b39355057a52139347f77aa348f
Alif0007 <alaminalif373@gmail.com>
2023-12-11 12:25:25 +0600

    path changed after login to getinto dashboard

--- Login/login.js
+++ Login/login.js
@@ -8,7 +8,7 @@ loginButton.addEventListener('click', () => {
     const userPasswordValue = passwordField.value.trim()
 
     if (userNameValue === 'alif' && userPasswordValue === 'alif07') {
-        window.location.href = '/bank-dashboard/Dashboard/dashboard.html'
+        window.location.href = 'https://alif0007.github.io/bank-dashboard/Dashboard/dashboard.html'
     } else {
         loginErrorMessege.innerHTML = "Invalid username or password, TRY AGAIN"
     }
